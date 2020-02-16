5b3c299b27cc18ca1477579a93139e68545ecd99
Georgge <jo123.garcia.s@gmail.com>
2020-02-16 13:15:26 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'https://rem-rest-api.herokuapp.com/api/';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
